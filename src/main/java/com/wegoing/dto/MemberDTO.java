8a068bba14cba006ba9b60d6ec4bce38131bcdc7
경제목 <jemok9605@naver.com>
2023-01-26 00:45:19 +0900

    [docs]- 불필요한 내용 삭제

--- src/main/java/com/wegoing/dto/MemberDTO.java
+++ src/main/java/com/wegoing/dto/MemberDTO.java
@@ -8,16 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class MemberDTO {
-	private String email;
-	private String name;
-	private String nickname;
-	private String pw;
-	private String tel;
-	private String image;
-	private String rank;
-	private Date join_dt;
+	
 }
