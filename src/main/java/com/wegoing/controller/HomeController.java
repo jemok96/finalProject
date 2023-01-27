662de472bd43470c9098c676987bbaa122908b6f
ODY <dy7133@naver.com>
2023-01-27 15:47:25 +0900

    mainPage중간commit 23/01//27

--- src/main/java/com/wegoing/controller/HomeController.java
+++ src/main/java/com/wegoing/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.wegoing.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -13,5 +14,15 @@ public class HomeController {
 	@GetMapping("/main")
 	public String main() {
 		return "home/mainpage";
+	}
+	
+	@GetMapping("/partner")
+	public String partnerForm() {
+		return "partner/partnerForm";
+	}
+	
+	@PostMapping("/partner")
+	public String submitForm() {
+		return "partner/partnerForm";
 	}
 }
