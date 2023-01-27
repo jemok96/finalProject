d4da6ea0419b5d2d200274c8277c919102b24c38
경제목 <jemok9605@naver.com>
2023-01-27 14:56:29 +0900

    [test]:게시판 insert, delete 테스트 코드

--- src/test/java/com/wegoing/dao/BoardTest.java
+++ src/test/java/com/wegoing/dao/BoardTest.java
@@ -2,12 +2,14 @@ package com.wegoing.dao;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.wegoing.dao.BoardDao;
 import com.wegoing.dto.BoardDTO;
+import com.wegoing.dto.MemberDTO;
 import com.wegoing.service.BoardService;
 
 
@@ -16,7 +18,14 @@ class BoardTest {
 	
 	@Autowired
 	BoardService boardService;
+
 	// Test시 메서드 이름에 올려놓고 ctrl + F11해야 그것만 실행됨
+	
+	@BeforeEach
+	void deleteAll() {
+		boardService.deleteAll();
+	}
+	
 	@Test
 	void insertTest() {
 		BoardDTO dto =
@@ -26,8 +35,22 @@ class BoardTest {
 		assertTrue(boardService.insert(dto) == 1);
 	}
 	@Test
-	void deleteCategorySelect() {
-		System.out.println(boardService.deleteCategoryNum(2));;
+	void insetTest2(){
+		BoardDTO[] dto = new BoardDTO[100];
+		for(int i=0; i<dto.length; i++) {
+			dto[i] = new BoardDTO("rudnf9605@naver.com", 4, "testTitle", "titleContent", "testNickname");
+			boardService.insert(dto[i]);
+		}
+		assertTrue(boardService.countBoard(4) == 100);
+		assertTrue(boardService.deleteAll() == 100);
+	}
+	
+	@Test
+	void deleteCategory() {
+		BoardDTO dto =
+				new BoardDTO("rudnf9605@naver.com", 2, "testTitle", "titleContent", "testNickname");
+		boardService.insert(dto);
+		assertTrue(boardService.deleteCategory(2) == 1);
 	}
 
 
