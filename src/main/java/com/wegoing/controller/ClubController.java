1b08796fdaf995cd910f4c02e07872d72661c29d
seh <02milkyway13@naver.com>
2023-02-02 11:30:59 +0900

    [feature] 협업공간 정보 수정 기능 추가

--- src/main/java/com/wegoing/controller/ClubController.java
+++ src/main/java/com/wegoing/controller/ClubController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -71,6 +72,35 @@ public class ClubController {
 		  } else {
 		    return "redirect:/main";
 		  }
+	}
+	
+	@GetMapping("/club/{clno}")
+	public String clubMain(@PathVariable int clno, Model model) {
+		// 협업공간 이름, 설명, 멤버
+		// 협업공간 생성자(host)와 사용자(user.mail)이 동일하면 설정 버튼 출력
+		ClubDTO cdto = cs.getOne(clno);
+		log.info("cdto >>>>> " + cdto);
+		model.addAttribute("club", cdto);
+		
+		return "club/clubMain";
+	}
+	
+	@PostMapping("/club/{clno}/edit")
+	public String editClub(@PathVariable int clno, @RequestParam("editClname")String editClname,
+							@RequestParam("editClexplain")String editClexplain, Model model) {
+		log.info("clno >>>>>" + clno);
+		ClubDTO cdto = cs.getOne(clno);
+		model.addAttribute("club", cdto);
+		
+		cdto.setClname(editClname);
+		cdto.setClexplain(editClexplain);
+		
+		log.info("수정 후 이름 >>>>>" + editClname);
+		log.info("수정 후 내용 >>>>>" + editClexplain);
+		
+		cs.modifyOne(cdto);
+		
+		return "redirect:/club/{clno}";
 	}
 
 	
