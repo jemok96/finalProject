b782732f9598a4fe785799d5797f450effa0c176
seh <02milkyway13@naver.com>
2023-02-06 14:17:58 +0900

    [merge]

--- src/main/java/com/wegoing/dao/DocumentDAO.java
+++ src/main/java/com/wegoing/dao/DocumentDAO.java
@@ -4,8 +4,10 @@ package com.wegoing.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+import com.wegoing.dto.BoardDTO;
 import com.wegoing.dto.DocumentDTO;
 import com.wegoing.dto.StartEnd;
 
@@ -19,4 +21,7 @@ public interface DocumentDAO {
 	public void updateOne(DocumentDTO dto);
 	public void deleteOne(int dno);
 	public void insertOne(DocumentDTO dto);
+	public List<DocumentDTO> selectAllList(int clno);
+	public List<DocumentDTO> selectDstatusList(@Param("dstatus")String dstatus,@Param("clno") int clno);
+	public void updateDstatus(@Param("dstatus")String dstatus,@Param("dno") int dno);
 }
