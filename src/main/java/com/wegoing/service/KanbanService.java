8fd005c72022eda018f30dcc84d12bdec5834ac4
ODY <dy7133@naver.com>
2023-02-06 11:43:53 +0900

    [feature] kanban기능 추가

---
+++ src/main/java/com/wegoing/service/KanbanService.java
@@ -0,0 +1,31 @@
+package com.wegoing.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wegoing.dao.DocumentDAO;
+import com.wegoing.dto.BoardDTO;
+import com.wegoing.dto.DocumentDTO;
+
+
+@Service
+public class KanbanService {
+	private final DocumentDAO dao;
+	
+	@Autowired
+	public KanbanService(DocumentDAO dao) {
+		this.dao = dao;
+	}
+
+//	public List<DocumentDTO> getAllList(int clno) {
+//		return dao.selectAllList(clno);
+//	}
+
+	public List<DocumentDTO> getDstatusList(String dstatus, int clno) {
+		return dao.selectDstatusList(dstatus, clno);
+	}
+
+	
+}
